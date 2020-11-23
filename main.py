02ec974d24fa083c1e80f9f12eb1c4d10fc65745
naokiohno <ohnonaoki95@gmail.com>
2020-11-23 21:34:37 +0000

    fetch and load data

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 
+import os
+import tarfile
 import pandas as pd
+from six.moves import urllib
 
-print("Hello world!")
+download_root = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+housing_path = os.path.join("datasets", "housing")
+housing_url = download_root + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=housing_url, housing_path=housing_path):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+def load_housing_data(housing_path=housing_path):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return(pd.read_csv(csv_path))
+
+fetch_housing_data()
+housing = load_housing_data()
+
+housing.head()
