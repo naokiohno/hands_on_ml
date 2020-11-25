bad7b0e217925c2d3290a79c812b1b82578b58c6
naokiohno <ohnonaoki95@gmail.com>
2020-11-25 14:13:27 +0000

    changes

--- eda.py
+++ eda.py
@@ -1,3 +1,14 @@
 
+import matplotlib.pyplot as plt
+
 # Check the data
+test = housing.head()
 housing.head()
+
+housing.info()
+
+housing["ocean_proximity"].value_counts()
+
+test = housing.describe()
+
+housing.hist(bins=50)
